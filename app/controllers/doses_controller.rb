fb382ef6dc7625e2eb762b2413fbc57e0b313aa4
Stephanie Opdam <scm.opdam@gmail.com>
2020-01-27 17:06:18 +0000

    Added random photos and basic recipe listing functionality

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,19 @@
+class DosesController < ApplicationController
+  def new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new
+  end
+
+  def create
+    @dose = Dose.new(dose_params)
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose.cocktail = @cocktail
+    @dose.save
+    redirect_to cocktail_path(@cocktail)
+  end
+
+  private
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
+  end
+end
