63dc23ee3b1793c9497c01c7b2f8d2f1b990c257
Stephanie Opdam <scm.opdam@gmail.com>
2020-01-28 21:11:50 +0000

    Finished user photo upload

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,5 +1,6 @@
 class Cocktail < ApplicationRecord
   validates :name, uniqueness:true, presence:true
+  has_one_attached :photo
   has_many :doses, dependent: :destroy
   has_many :ingredients, through: :doses
 end
